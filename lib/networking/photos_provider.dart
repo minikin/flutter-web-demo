d945fe95c80452c519c1b4fd87f16f4fe3130a6b
Sasha Prokhorenko <djminikin@gmail.com>
2019-05-14 11:51:58 +0200

    Update PhotoProvider

--- lib/networking/photos_provider.dart
+++ lib/networking/photos_provider.dart
@@ -4,17 +4,19 @@ import 'package:built_collection/built_collection.dart';
 import 'package:flutter_web/foundation.dart';
 import 'package:flutter_web_demo/models/models.dart';
 import 'package:flutter_web_demo/networking/photo_error.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart' show Client;
 
 class PhotoProvider {
-  final _baseUrl = 'https://picsum.photos/v2/';
-  final _client = http.Client();
+  final _baseUrl = 'https://picsum.photos/v2';
+  final _client = Client();
 
   Future<Photo> fetchPhoto({
     @required String photoId,
+    @required int width,
+    @required hight,
   }) async {
     final response = await _client.get(
-      '$_baseUrl/$photoId',
+      '$_baseUrl/id/$photoId/$width/$hight',
       headers: {
         HttpHeaders.acceptHeader: '*/*',
         HttpHeaders.cacheControlHeader: 'no-cache',
@@ -33,7 +35,7 @@ class PhotoProvider {
     @required String page,
   }) async {
     final response = await _client.get(
-      '$_baseUrl/questionnaires',
+      '$_baseUrl/list?page=$page&limit=50',
       headers: {
         HttpHeaders.acceptHeader: '*/*',
         HttpHeaders.cacheControlHeader: 'no-cache',
